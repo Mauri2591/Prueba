c923c4cfe13a59618f13686af682992809b8fffd
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-07-20 14:39:06 -0300

    Se actualiza codigo

--- users/usuarios.php
+++ users/usuarios.php
@@ -3,6 +3,6 @@
     if($_SERVER['REMOTE_ADDR'] != '127.0.0.1'){
         die ("Acceso no autorizado");
     }else{
-        echo 'FLAG{'.rand().'}';
+        echo 'FLAG{'.rand(md5(uniqid(),true)).'}';
     }
 ?>
